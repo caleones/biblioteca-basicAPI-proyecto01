2f75f9a583a196dda8ab74d8770ce37d529cf661
Carlos Alberto Leones Rivera <carlos_leones4@hotmail.com>
2025-05-02 20:31:04 +0000

    feat: conexión a MongoDB usando Mongoose

---
+++ src/database.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+// Se intenta conectar a MongoDB con Mongoose
+async function connectDB() {
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("Conectado a MongoDB");
+  } catch (err) {
+    console.error("Error al conectar a MongoDB:", err);
+    process.exit(1);
+  }
+}
+
+module.exports = connectDB;
--- src/server.js
+++ src/server.js
@@ -1,24 +1,28 @@
 const express = require("express");
 const dotenv = require("dotenv");
 
-// cargar las variables de entorno desde el  archivo .env
+const connectDB = require("./database");
+
+// Cargar las variables de entorno desde el archivo .env
 dotenv.config();
 
-// inicializar la app Express
+// Inicializar la app Express
 const app = express();
 
-// interpreta peticiones JSON con Express
+// Interpreta las peticiones JSON con Express
 app.use(express.json());
 
-// ruta de prueba de API
+// Ruta de prueba de API
 app.get("/", (req, res) => {
   res.status(200).json({ message: "API Biblioteca funcionando" });
 });
 
-// puerto definido en .env o el puerto por defecto (3000)
+// Se define el puerto desde .env o con el puerto por defecto
 const PORT = process.env.PORT || 3000;
 
-// inicializar el servidor
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en puerto ${PORT}`);
+// Intenta conectar a MongoDB para inicializar el servidor
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log(`🚀 Servidor corriendo en puerto ${PORT}`);
+    });
 });
